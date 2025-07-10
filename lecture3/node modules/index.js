2b90b22036f95472e7312a74f7e5cc03dd707209
Sajal68 <sajalgupta1742@gmail.com>
2025-07-10 15:06:42 +0530

    hi hello

---
+++ lecture3/node modules/index.js
@@ -0,0 +1,3 @@
+const express = require("express"); //no need to provicde the path, as it is installed in node_modules
+
+module.exports = require("./lib/sum.js");
---
+++ lecture3/nodejs/index.js
@@ -0,0 +1,2 @@
+let mypackage = require("@cuiet.sajal/myfirstpackage");
+console.log(mypackage.sum(2, 5));
