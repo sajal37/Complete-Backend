c7b514f038ec5345b9c3e96e43ebcb84188fa41f
Sajal68 <sajalgupta1742@gmail.com>
2025-07-24 15:14:21 +0530

    ok

--- lecture10/server.js
+++ lecture10/server.js
@@ -1,6 +1,26 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const path = require("path");
+const app = express();
+
 app.use(express.json());
-app.listen(3000,()=>{
-    console.log("server started");
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static("C:/Users/lenovo/OneDrive/Documents/Full Stack/CSS Projects/Login Page"));
+
+app.get("/", (req, res) => {
+  res.sendFile("C:/Users/lenovo/OneDrive/Documents/Full Stack/CSS Projects/Login Page/login.html");
+});
+
+app.post("/", (req,res)=> {
+    const newUser={
+        email: req.body.email,
+        password: req.body.password
+    }
+    console.log("Login Details: "+JSON.stringify(newUser));
+    res.send("Login successful");
 })
+
+app.listen(3000, () => {
+  console.log("server started");
+});
