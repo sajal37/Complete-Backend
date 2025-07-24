563f78a617255a56b9ac765344345ebceade6c05
Sajal68 <sajalgupta1742@gmail.com>
2025-07-24 15:33:23 +0530

    ok

--- lecture10/server.js
+++ lecture10/server.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(express.static("C:/Users/lenovo/OneDrive/Documents/Full Stack/CSS Projects/Login Page"));
+app.use(
+  express.static(
+    "C:/Users/lenovo/OneDrive/Documents/Full Stack/CSS Projects/Login Page"
+  )
+);
 
 app.get("/", (req, res) => {
-  res.sendFile("C:/Users/lenovo/OneDrive/Documents/Full Stack/CSS Projects/Login Page/login.html");
+  res.sendFile(
+    "C:/Users/lenovo/OneDrive/Documents/Full Stack/CSS Projects/Login Page/login.html"
+  );
 });
 
-app.post("/", (req,res)=> {
-    const newUser={
-        email: req.body.email,
-        password: req.body.password
+app.post("/", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  fs.readFile("data.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
     }
-    console.log("Login Details: "+JSON.stringify(newUser));
-    res.send("Login successful");
-})
+      let users;
+      if (data) {
+        users = JSON.parse(data);
+      } else {
+        users = [];
+      }
+      users.push(newUser);
+      fs.writeFile("data.json", JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+            res.send("Login successful");
+        }
+      });
+    }
+  );
+  console.log("Login Details: " + JSON.stringify(newUser));
+});
 
 app.listen(3000, () => {
   console.log("server started");
