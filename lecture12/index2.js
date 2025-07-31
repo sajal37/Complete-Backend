b2165ebf874b90a0263e8c2da8370946f3a9e861
Sajal68 <sajalgupta1742@gmail.com>
2025-07-31 14:25:49 +0530

    ok

---
+++ lecture12/index2.js
@@ -0,0 +1,4 @@
+let edit = document.getElementsByClassName("edit");
+console.dir(edit);
+let del = document.getElementsByClassName("delete");
+console.log(del);
---
+++ lecture13/index3.js
@@ -0,0 +1,33 @@
+let todos = [
+  {
+    id: 1234,
+    title: "Learn JavaScript",
+  },
+  {
+    id: 1235,
+    title: "Learn React",
+  },
+];
+let ul = document.querySelector(".list");
+function createTodoELement() {
+  let li = document.createElement("li");
+  li.setAttribute("id", `${todo.id}`);
+  li.innerHTML = `<div>
+                <input type="checkbox" name="" id="checkbox">
+                <h1>${todo.title}</h1>
+                <div>
+                    <button class="edit">edit</button>
+                    <button class="delete">delete</button>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum adipisci aperiam, eaque est officiis facilis voluptas alias iusto quam nihil tenetur porro. Veritatis beatae vitae dolore consectetur! Soluta, officia provident.</p>
+                </div>
+            </div>`;
+  ul.appendChild(li);
+}
+
+function showAllTodos(todos) {
+  todos.forEach((todo) => {
+    createTodoELement(todo);
+  });
+}
+
+showAllTodos(todos);
