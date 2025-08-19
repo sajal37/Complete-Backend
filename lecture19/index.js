29e295198ada19964993627da92648c34cdbacbc
Sajal68 <sajalgupta1742@gmail.com>
2025-08-19 10:06:31 +0530

    ok

---
+++ lecture19/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Blog = require("./model/blog");
+const User = require("./model/user");
+let blogRoutes=require("./routes/blogRoutes");
+let userRoutes=require("./routes/userRoutes");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api/blogs", blogRoutes);
+app.use("/api/users", userRoutes);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/G27DBs")
+  .then(() => console.log("Connected"));
+app.listen(5556, () => {
+  console.log("server started");
+});
