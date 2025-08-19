487251f04313751b672139f668503b7344b53769
Sajal68 <sajalgupta1742@gmail.com>
2025-08-19 10:36:08 +0530

    ok

--- lecture19/routes/userRoutes.js
+++ lecture19/routes/userRoutes.js
@@ -1,37 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../model/user");
+let usercontroller = require("../controller/usercontroller");
 
-router.post("/", async (req, res) => {
-  let { username, email, password } = req.body;
-  let newUser = new user({
-    username,
-    email,
-    password,
-  });
-  await newUser.save();
-  res.json({
-    success: true,
-    data: newUser,
-    message: "blog added successfully",
-  });
-});
-router.get("/", async (req, res) => {
-  let allusers = await user.find();
-  res.json({
-    success: true,
-    data: allusers,
-  });
-});
-router.get("/:id", async (req, res) => {
-  let { id } = req.params;
-  let userExist = await user.findOne({ _id: id }).populate("blogs");
-  if (userExist) {
-    res.json({
-      success: true,
-      data: userExist,
-    });
-  }
-});
+router.post("/", usercontroller.postAddUser);
+router.get("/", usercontroller.getAllUser);
+router.get("/:id", usercontroller.getOneUser);
 
 module.exports = router;
