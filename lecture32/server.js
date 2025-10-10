b62e0a8b783da2df8e30bc830155817e49f1f33f
Sajal68 <sajalgupta1742@gmail.com>
2025-10-10 14:07:09 +0530

    ok

---
+++ lecture32/server.js
@@ -0,0 +1,15 @@
+const {WebSocketServer} = require('ws');
+const wss = new WebSocketServer({port: 8080});
+
+
+let rooms=new Map();
+// {
+//     "1234":[s1, s2, s3]
+// }
+wss.on("connection", function(socket){
+    console.log("a new user connected");
+    socket.on("message", function(message){
+        let parsedMessage=JSON.parse(message);
+        if(parsedMessage.type==="join"){
+    }})
+});j
